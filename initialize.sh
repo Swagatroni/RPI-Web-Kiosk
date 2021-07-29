143118b26433829602d871b9804f002fd2f4f8ff
Kenneth Thomas Jr <75589633+Swagatroni@users.noreply.github.com>
2021-07-29 14:02:10 -0500

    Create initialize.sh

---
+++ initialize.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get purge wolfram-engine scratch scratch2 nuscratch sonic-pi idle3 -y
+sudo apt-get purge smartsim java-common minecraft-pi libreoffice* -y
+
+sudo apt-get clean
+sudo apt-get autoremove -y
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install xdotool unclutter sed
+
+sudo raspi-config
+
+nano /home/pi/kiosk.sh
