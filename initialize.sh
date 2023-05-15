c55e4be52b8e1962332cde1e3c041f2a423d1cdc
Kenneth Thomas Jr <75589633+Swagatroni@users.noreply.github.com>
2023-05-15 13:43:24 -0500

    Update initialize.sh

--- initialize.sh
+++ initialize.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get upgrade
+
 sudo apt-get purge wolfram-engine scratch scratch2 nuscratch sonic-pi idle3 -y
 sudo apt-get purge smartsim java-common minecraft-pi libreoffice* -y
 
 sudo apt-get clean
 sudo apt-get autoremove -y
-
-sudo apt-get update
-sudo apt-get upgrade
 
 sudo apt-get install xdotool unclutter sed
 
